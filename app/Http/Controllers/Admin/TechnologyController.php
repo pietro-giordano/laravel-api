9acbb666cb1a2e665e56017aa0b4de35aac6f043
Pietro Giordano <giopi87@gmail.com>
2023-03-29 15:27:46 +0200

    Initial commit

---
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Technology;
+use App\Http\Requests\StoreTechnologyRequest;
+use App\Http\Requests\UpdateTechnologyRequest;
+// Helpers
+use Illuminate\Support\Str;
+
+class TechnologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $technologies = Technology::all();
+        return view('admin.technologies.index', compact('technologies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.technologies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTechnologyRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTechnologyRequest $request)
+    {
+        $data = $request->validated();
+        $data['slug'] = Str::slug($data['name']);
+
+        $newTechnology = Technology::create($data);
+        return redirect()->route('admin.technologies.show', $newTechnology)->with('success', 'Tecnologia aggiunta con successo');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Technology $technology)
+    {
+        return view('admin.technologies.show', compact('technology'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Technology $technology)
+    {
+        return view('admin.technologies.edit', compact('technology'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTechnologyRequest  $request
+     * @param  \App\Models\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTechnologyRequest $request, Technology $technology)
+    {
+        $data = $request->validated();
+        $data['slug'] = Str::slug($data['name']);
+
+        $technology->update($data);
+        return redirect()->route('admin.technologies.show', $technology->id)->with('success', 'Tecnologia aggiornata con successo');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Technology $technology)
+    {
+        //
+    }
+}
